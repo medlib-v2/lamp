3e9ddb2cb1e9fe36647dda6e77bd451a350eaa0f
Patrick Luzolo <eldorplus@gmail.com>
2015-10-23 15:47:50 +0200

    fixed mysql

--- site-cookbooks/app/recipes/db.rb
+++ site-cookbooks/app/recipes/db.rb
@@ -7,9 +7,9 @@
 # Create database if it doesn't exist
 ruby_block "create_#{node['app']['name']}_db" do
     block do
-        %x[mysql -uroot -e "CREATE DATABASE #{node['app']['db_name']};"]
+      %x[mysql -uroot -p#{node['mysql']['server_root_password']} -e "CREATE DATABASE #{node['app']['db_name']};"]
     end
-    not_if "mysql -uroot -e \"SHOW DATABASES LIKE '#{node['app']['db_name']}'\" | grep #{node['app']['db_name']}";
+    not_if "mysql -uroot -p#{node['mysql']['server_root_password']} -e \"SHOW DATABASES LIKE '#{node['app']['db_name']}'\" | grep #{node['app']['db_name']}";
     action :create
 end
 
@@ -17,9 +17,9 @@ end
 if File.exist?("#{node['app']['db_dump']}")
     ruby_block "seed #{node['app']['name']} database" do
         block do
-            %x[mysql -u root #{node['app']['db_name']} < #{node['app']['db_dump']}]
+            %x[mysql -u root -p#{node['mysql']['server_root_password']} #{node['app']['db_name']} < #{node['app']['db_dump']}]
         end
-        not_if "mysql -u root -e \"SHOW TABLES FROM #{node['app']['db_name']}\" | \
+        not_if "mysql -u root -p#{node['mysql']['server_root_password']} -e \"SHOW TABLES FROM #{node['app']['db_name']}\" | \
             grep 1"
         action :create
     end
